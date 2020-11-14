803c808f64ee448740a457a7f7ba36758171fe92
przemyslavbartosz <48643531+przemyslavbartosz@users.noreply.github.com>
2020-11-15 00:11:08 +0100

    Initial commit

---
+++ NewBrandingStyle.web/Controllers/CompanyController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyle.web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Index(CompanyModel company)
+        {
+            var ViewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+
+            return View("CompanyAdded", ViewModel);
+        }
+
+    }
+}
---
+++ NewBrandingStyle.web/Models/CompanyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewBrandingStyle.web.Models
+{
+    public class CompanyModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
